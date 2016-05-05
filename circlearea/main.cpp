5af0c5aae71a5024a1d3dd82cf8f77f1f1e4c42a
luojingzhao <1241138441@qq.com>
2016-05-05 23:53:23 +0800

    circle

---
+++ circlearea/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"print.h" 
+using namespace std;
+int main()
+{
+	double r;
+	cout << "Please input the radius of a circle : ";
+	cin >> r;
+	Printf(r);
+	return 0;
+}
+
---
+++ circlearea/print.h
@@ -0,0 +1,11 @@
+#include<iostream>
+#define pi 3.1415926
+using namespace std;
+ 
+void Printf(double r)
+{
+	cout <<"Answer:" ;
+	cout << pi*r*r << endl;
+} 
+
+
