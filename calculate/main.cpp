5ff521762c0f53319792323740f7581f0d8f5ab2
luojingzhao <1241138441@qq.com>
2016-05-10 23:51:31 +0800

    code finish

--- calculate/main.cpp
+++ calculate/main.cpp
@@ -26,49 +26,7 @@ using namespace std;
 
 
 int main(int argc, char* argv[]) 
-<<<<<<< 5af0c5aae71a5024a1d3dd82cf8f77f1f1e4c42a
 {
-    Dispose *p = new Dispose();  /*申请内存 */
-    Print *t = new Print();
-	Calculation *cal=new Calculation();     
-    string _input, strGetInputRet;    /*实例化string*/ 
-    
-    cout<<"输入四则运算式：" ;
-    if(argc<2)
-    {
-    	cout << "无参数" << endl; 
-    }
-    else
-	{
-	    if(strcmp(argv[1],"-a") == 0) //判断是否要输出表达式 
-	    {
-	    	cout<<argv[2]<<"=";
-	    	_input=argv[2];
-			p->setinput(_input);
-	    }
-	    else
-	    {
-	    	_input=argv[1];
-	    	p->setinput(_input);
-	    }
-	    strGetInputRet = p->getinput();
-	    
-	    double calculate;
-	    calculate = cal->CaculateExpression(p->ToStringQueue(strGetInputRet)); //传入队列进行计算 
-		cout<<"答案："; 
-		t->output(calculate);  //打印答案 
-	    
-	    /*释放内存 ，并使其指向空指针*/ 
-	    delete p;   
-	    p=NULL;
-	    delete t;
-	    t=NULL;
-	    delete cal;
-	    cal=NULL;
-    }
-=======
-{     
-
 	if(argc<2)
 	{
 		cout<<"无参数"<<endl;
@@ -127,6 +85,5 @@ int main(int argc, char* argv[])
 	    }
 	}
 		 
->>>>>>> second finish
     return 0;
 }
